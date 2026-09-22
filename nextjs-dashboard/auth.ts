036cbcff185f330456a16892d5c03a1a30f09bf3
maha404 <maria_halvarsson@hotmail.se>
2026-09-22 22:05:03 +0200

    chore: add bcrypt and its types for password hashing functionality
    - Added bcrypt package (version 6.0.0) to package.json for secure password hashing.
    - Included @types/bcrypt (version 6.0.0) in devDependencies for TypeScript support.
    - Updated pnpm-lock.yaml to reflect the addition of bcrypt and its types.

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -3,7 +3,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import { compare } from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import postgres from 'postgres';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -31,7 +31,9 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
-          const passwordsMatch = await compare(password, user.password);
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
         }
  
         return null;
